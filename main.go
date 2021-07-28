e9c5fb3867d1478702a350414f515994de713c34
Regi Arjuna Purba <regipurba07@gmail.com>
2021-07-28 09:53:01 +0700

    add root path

--- main.go
+++ main.go
@@ -12,11 +12,18 @@ import (
 	"go.uber.org/zap"
 )
 
+func Hello(c *gin.Context) {
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"data": "Hello World",
+	})
+}
+
 func main() {
 	config.LoadConfig()
 	log, _ := zap.NewDevelopment()
 	client := &http.Client{}
 	r := gin.Default()
+	r.GET("/", Hello)
 	v1 := r.Group("/v1")
 	moviesRepo := moviesRepo.NewMoviesRepository(database.GetConnection(), client, log)
 	moviesUsecase := moviesUsecase.NewMoviesUseCaSe(moviesRepo)
